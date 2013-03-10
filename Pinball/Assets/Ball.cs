94053a4eaf3e3ce1a25b56402666a968e4466651
dmitry-sjsu <dima.makovsky@gmail.com>
2013-03-10 13:28:37 -0700

    Launcher tweaks
    Added a spring to the launcher
    Made the table walls transparent again (so you can actually see the
    ball launch)
    Adjusted the ball's starting position and mass (so that it wouldn't
    move the launcher down)

--- Pinball/Assets/Ball.cs
+++ Pinball/Assets/Ball.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class Ball : MonoBehaviour {
 	
-	Vector3 ballVelocity = new Vector3 (0f, .5f, 0f);
+	Vector3 ballVelocity = new Vector3 (0f, 0f, 0f);
 	private int numBallsUsed = 0;
 
 	
 	// Use this for initialization
 	void Start () {
-		this.transform.position = new Vector3 (0.85f, 0f, -.05f);
+		this.transform.position = new Vector3 (0.85f, -.3f, -.05f);
 		this.gameObject.rigidbody.velocity = ballVelocity;
 		GameObject.FindGameObjectWithTag("BallsLeft").guiText.text = 				
 					numBallsUsed.ToString();
